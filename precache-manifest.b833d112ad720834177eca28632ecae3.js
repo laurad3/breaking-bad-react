74cfaf7130a12a3955490ad56d0c6fcaea2f626b
laurad3 <lauralvicuna@gmail.com>
2020-08-09 20:56:46 +0300

    Updates

---
+++ precache-manifest.b833d112ad720834177eca28632ecae3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f17610bc9551ca733ab16577609621b",
+    "url": "/breaking-bad-react/index.html"
+  },
+  {
+    "revision": "789f1cdf0a1fe464a5f3",
+    "url": "/breaking-bad-react/static/css/main.b78c1d14.chunk.css"
+  },
+  {
+    "revision": "c26ec6b6d34502fd0b56",
+    "url": "/breaking-bad-react/static/js/2.d4fe6915.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/breaking-bad-react/static/js/2.d4fe6915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "789f1cdf0a1fe464a5f3",
+    "url": "/breaking-bad-react/static/js/main.4abbc1a6.chunk.js"
+  },
+  {
+    "revision": "23a8f4d40c65217596b8",
+    "url": "/breaking-bad-react/static/js/runtime-main.37ff251b.js"
+  },
+  {
+    "revision": "84d3c4b00b0b2374e594bce623f90461",
+    "url": "/breaking-bad-react/static/media/background.84d3c4b0.jpg"
+  }
+]);
